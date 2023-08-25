4591200e2c3bcd16c5c6a472624e1c823e87e0fe
Micah <micahshallom@gmail.com>
2023-08-25 02:14:22 +0100

    restructured User and Login methods to use classmethods

--- user.py
+++ user.py
@@ -1,39 +1,48 @@
+import hashlib, random
+
 class User:
     registered_users = dict()
+    user_id_counter = 1
 
-    def __init__(self, user_id=None, username=None, email=None, password=None):
-        self.user_id = user_id
+    def __init__(self,password=None, user_id=None, username=None, email=None):
+        # self.user_id = user_id
         self.username = username
         self.email = email
-        self.password = password
+        self.password_hash = hashlib.sha256(password.encode()).hexdigest()
+        User.registered_users[username] = self
+        User.user_id_counter += 1
 
-    def register(self,username,email,password):
-        if username in User.registered_users:
+    def verify_password(self,password):
+        return self.password_hash == hashlib.sha256(password.encode()).hexdigest()
+
+    @classmethod
+    def register(cls,username,email,password):
+        if username in cls.registered_users:
             return "User already exits. Proceed to Login"
         else:
-            User.registered_users[username] = {
-                "userID": len(User.registered_users) + 1,
-                "email": email,
-                "password": password
-            }
+            new_user = cls(username,email,password)
+            # User.registered_users[username] = {
+            #     "userID": len(User.registered_users) + 1,
+            #     "email": email,
+            #     "password": password
+            # }
             return "Registration successful. You can now login"
     
-    def login(self,username,password):
-        if username in User.registered_users:
-            if password == User.registered_users[username]["password"]:
-                print("Login Successful")
+    @classmethod
+    def login(cls,username,password):
+        if username in cls.registered_users:
+            user = cls.registered_users[username]
+            if user.verify_password(password):
                 return "Login Successful"
             else:
                 return "Incorrect Password. Please try again"
         else: return "Username not found!! Register user"
 
-user = User()
-print(user.registered_users)
 
 # Register new users and display registration/login messages
-print(user.register("alice", "alice@example.com", "password123"))
-print(user.register("bob", "bob@example.com", "pass456"))
-print(user.login("alice", "password123"))
-print(user.login("bob", "wrongpassword"))
-print(user.login("carol", "somepassword"))
+print(User.register("alice", "alice@example.com", "password123"))
+print(User.register("bob", "bob@example.com", "pass456"))
+print(User.login("alice", "password123"))
+print(User.login("bob", "wrongpassword"))
+print(User.login("carol", "somepassword"))
  
